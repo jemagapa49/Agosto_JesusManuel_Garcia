7559b3d6f9f311326197ccb238c121f12497946f
jemagapa49@gmail.com <jemagapa49@gmail.com>
2020-08-30 18:17:22 -0300

    Ejercicio 1 de la primera clase

---
+++ ejercicio1.java
@@ -0,0 +1,9 @@
+package Practico2;
+
+public class ejercicio1 {
+
+    public static void main (String args []) {
+        System.out.println ("hola mundo");
+        System.out.println ("ahora si anda");
+    }
+}
